4319fd15f2e1dcc262a02a9ff49f282672e6a44f
he1a2s0 <he1a2s0@gmail.com>
2019-09-22 02:37:49 +0800

    015.3Sum

---
+++ 015.3Sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _015._3Sum
+{
+    class Program
+    {
+        private readonly static Solution _solution = new Solution();
+
+        static void Main(string[] args)
+        {
+            Assert(
+                new int[] { -1, 0, 1, 2, -1, -4 },
+                new List<IList<int>>{
+                    new List<int>{-1,-1,2},
+                    new List<int>{-1,0,1}
+                }
+                );
+        }
+
+        static void Assert(int[] nums, IList<IList<int>> expected)
+        {
+            var actual = _solution.ThreeSum(nums);
+
+            var eq = NestedListsEquals(actual, expected);
+            Console.WriteLine(
+                "[{0}]\t\t=>\t{1}\t{2}",
+                string.Join(",", nums),
+                eq ? "√" : "x",
+                !eq ?
+                    "[" + string.Join("],[", actual.Select(_ => string.Join(",", _))) + "]" :
+                    string.Empty);
+        }
+
+        private static bool NestedListsEquals(IList<IList<int>> actual, IList<IList<int>> expected)
+        {
+            if (actual.Count != expected.Count) return false;
+            for (var i = 0; i < actual.Count; i++)
+            {
+                if (!actual[i].SequenceEqual(expected[i]))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ 015.3Sum/README.md
@@ -0,0 +1,23 @@
+
+# 3Sum (Difficulty: **_Medium_**)
+
+link: https://leetcode.com/problems/3sum
+
+Given an array `nums` of n integers, are there elements a, b, c in `nums` such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+**Note:**
+
+The solution set must not contain duplicate triplets.
+
+**Example:**
+
+    Given array nums = [-1, 0, 1, 2, -1, -4],
+
+    A solution set is:
+    [
+        [-1, 0, 1],
+        [-1, -1, 2]
+    ]
+
+
+[submission detail on leetcode](https://leetcode.com/submissions/detail/262902554/)
